507af276b973d71d8609f55dd9ccf499c3d91f33
cadlaburch <95720318+cadlaburch@users.noreply.github.com>
2022-03-06 22:40:55 -0800

    Add files via upload

---
+++ FirstLook.R
@@ -0,0 +1,9 @@
+#This file is a first look into the data
+library(tidyverse)
+
+#Import Data
+#I've only downloaded data from the GOA
+
+GOA_PL<- read_csv("Data/1_Gulf Of Alaska_Raw_PreyLength.csv")
+GOA_SC<- read_csv("Data/1_Gulf Of Alaska_Raw_StomachContents.csv")
+
