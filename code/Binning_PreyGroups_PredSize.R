e7e6d585e4208f1a7613c63fac145729c46a22c6
catalina <95720318+cadlaburch@users.noreply.github.com>
2022-08-30 13:41:10 -0700

    Create new file for binning

---
+++ code/Binning_PreyGroups_PredSize.R
@@ -0,0 +1,26 @@
+###########
+#Prey Grouping
+#In order to create figures I must bin the Prey into groups
+#Buckley et al. 2016 looked at pollock and grouped prey into 10 common prey groups
+#Fishes, Larvaceans, Chaetognaths, Shrimps, Euphausiids, Gammarids, Mysids, Copepods, Other
+
+#Barnes et al. 2018 looked at A flounder and Halibut and grouped prey into 33 groups
+
+#Yang et al. 2006 grouped prey differently for each commercial species
+##Pollock: Misc. prey, Calanoid, Amphipod, Euphausiid, Shrimp, Misc. fish, Osmeridae, Pollock
+##Cod: Misc. prey, Polychaete, Euphasiid, Shrimp, Tanner crab, other crab, Misc. fish, Flatfish, pollock
+#A Flounder: Misc. Prey, Euphausiid, Shrimp, Misc. fish, Ammodytidae, Osmeridae, Pollock
+#Halibut: Misc. Prey, Other crab, Shrimp, Hermit crab, Tanner crab, Misc. fish, Pollock
+#Sablefish: Misc. prey, Jellyfish, Cephalopod, Shrimp, Crab, misc. fish, pollock, fishery offal
+###The other commercial species didn't have stacked bar charts, only tables
+
+#The raw data has 90 groupings
+
+#############
+#Spatial Grouping
+#Yang grouped the study area into West, Central and East GOA
+#Barnes et al 2020 grouped into both West, Central, East GOA and the 5 INPFC subregions
+
+
+#############
+#Species size classes
