8fe02f0d5f099adc417c4e20190957faf4e239d1
catalina <95720318+cadlaburch@users.noreply.github.com>
2022-09-12 13:17:57 -0700

    figs
    starting

--- code/Binning_PreyGroups_PredSize.R
+++ code/Binning_PreyGroups_PredSize.R
@@ -14,12 +14,26 @@
 #Sablefish: Misc. prey, Jellyfish, Cephalopod, Shrimp, Crab, misc. fish, pollock, fishery offal
 ###The other commercial species didn't have stacked bar charts, only tables
 
+#Livingston et al 2017 showes an example of Pollock stomachs with four groups
+##copepods, euphausiids, pollock, and other
+
 #The raw data has 90 groupings
+
+#I would like to create a grouping based on Buckley's 10 common prey groups
+
+print(unique(raw_stomach_contents$Prey_Name))
+test <- raw_stomach_contents
+
+test = test %>% 
+  mutate(Buckley_Prey = case_when(Prey_Name == c("Misc Shrimp", "Crangonidae (shrimp)") ~ "Shrimp"))
 
 #############
 #Spatial Grouping
 #Yang grouped the study area into West, Central and East GOA
 #Barnes et al 2020 grouped into both West, Central, East GOA and the 5 INPFC subregions
+#Barnes et al 2018 grouped into the 5 INPFC statistical regions and the four IPHC regulatory areas
+# Buckley et al 2016 looks at the EBS and groups by sampling strata
+
 
 
 #############
